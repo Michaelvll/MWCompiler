50c9be4febbedad64995f7b59644dbd44aa003f6
Michael Wu <wz.wzh@qq.com>
2018-04-30 10:56:55 +0800

    Fix a bug for return type check

--- MWCompiler/src/mwcompiler/ir/nodes/BasicBlock.java
+++ MWCompiler/src/mwcompiler/ir/nodes/BasicBlock.java
@@ -6,4 +6,8 @@ import java.util.LinkedList;
 public class BasicBlock {
     private LinkedList<InstructionNode> instructions = new LinkedList<>();
 
+    public void insert(InstructionNode instruction) {
+        instructions.add(instruction);
+    }
+
 }
