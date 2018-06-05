a8bd3fffb90560556b64f738b503fdb9329bdb99
Michael Wu <wz.wzh@qq.com>
2018-06-05 21:56:09 +0800

    Turn on the memorize search

--- codegen.bash
+++ codegen.bash
@@ -1,5 +1,5 @@
 set -e
 cd "$(dirname "$0")"
 cat > program.in
-java -cp ./lib/*:./bin mwcompiler.Mwcc -i program.in -a Graph
+java -cp ./lib/*:./bin mwcompiler.Mwcc -i program.in -a Graph --memorize-search
 cat ./lib/lib.asm
