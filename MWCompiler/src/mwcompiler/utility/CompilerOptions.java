548937235141d1fadda11714a1c63ffc39c4bd20
Michael Wu <wz.wzh@qq.com>
2018-06-06 15:57:31 +0800

    inline : 4 10 5

--- MWCompiler/src/mwcompiler/utility/CompilerOptions.java
+++ MWCompiler/src/mwcompiler/utility/CompilerOptions.java
@@ -26,9 +26,9 @@ public class CompilerOptions {
     public boolean graphAllocate = false;
     public boolean functionInline = true;
     // Function inline
-    public final int INLINE_CALLEE_BOUND = 1 << 7;
+    public final int INLINE_CALLEE_BOUND = 1 << 4;
     public final int INLINE_CALLER_BOUND = 1 << 10;
-    public Integer INLINE_RECURSIVE_LEVEL = 8;
+    public Integer INLINE_RECURSIVE_LEVEL = 5;
 
     // Function memorize search
     public final int MEMORIZE_SEARCH_LEVEL = 1 << 8;
