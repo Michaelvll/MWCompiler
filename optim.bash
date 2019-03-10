d9c7ff612c3eee4df7f73682defb8f1149a13cfe
Michael Wu <wz.wzh@qq.com>
2019-03-10 11:01:01 +0800

    Fixed a typo for optim.bash

--- optim.bash
+++ optim.bash
@@ -1,5 +1,5 @@
 set -e
 cd "$(dirname "$0")"
 cat > program.in
-java -cp ./lib/*:./bin mwcompiler.Mwcc -i program.in -a Graph --memorize-search
+java -cp ./lib/*:./bin mwcompiler.Mwcc -i program.in -a Graph --memoized-search
 cat ./lib/lib.asm
